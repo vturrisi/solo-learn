a580b189673f21763ce0441f65cb7131d12d1b8e
Victor Turrisi <vt.turrisi@gmail.com>
2021-03-16 14:23:42 -0300

    working on simsiam

--- losses/neg_cosine_sim.py
+++ losses/neg_cosine_sim.py
@@ -7,4 +7,7 @@ def negative_cosine_similarity(p, z):
     p = F.normalize(p, dim=1)
     z = F.normalize(z, dim=1)
 
+    print(p * z.detach())
+    exit()
+
     return -(p * z.detach()).sum(dim=1).mean()
