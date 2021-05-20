e943515945fd63f751dad1bbfa45d476ef23faf1
Enrico Fini <enrico.fini@gmail.com>
2021-05-20 11:55:47 +0200

    fixed simsiam loss

--- losses/simsiam.py
+++ losses/simsiam.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 
 def simsiam_loss_func(p, z, simplified=True):
     if simplified:
-        return 2 - 2 * F.cosine_similarity(p, z.detach(), dim=-1).mean()
+        return -F.cosine_similarity(p, z.detach(), dim=-1).mean()
     else:
         p = F.normalize(p, dim=1)
         z = F.normalize(z, dim=1)
