e9e7441e89ff911ef6bc834e8f1028096d5abf31
Victor Turrisi <vt.turrisi@gmail.com>
2021-05-31 11:23:25 -0300

    added url to setup

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     license="MIT",
     author="Victor G. Turrisi da Costa, Enrico Fini",
     author_email="vturrisi@gmail.com, enrico.fini@gmail.com",
+    url="https://github.com/vturrisi/solo-learn",
     keywords=KW,
     install_requires=requirements,
     dependency_links=["https://developer.download.nvidia.com/compute/redist"],
