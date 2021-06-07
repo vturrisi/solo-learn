125e9586be42bb17075808d9fd217cf20d3f888e
Enrico Fini <enrico.fini@gmail.com>
2021-06-07 17:29:28 +0200

    added min lr and automatic scaling for accumulate grad (#41)
    * added min lr and automatic scaling for accumulate grad
    
    * added min lr to without warmup scheduler
    
    * removed warmup start from swav script
    
    * reverted unwanted changes to swav linear script

--- bash_files/pretrain/swav.sh
+++ bash_files/pretrain/swav.sh
@@ -14,7 +14,8 @@ python3 ../../main_contrastive.py \
     --exclude_bias_n_norm \
     --scheduler warmup_cosine \
     --lr 0.6 \
-    --classifier_lr 0.03 \
+    --min_lr 0.0006 \
+    --classifier_lr 0.1 \
     --weight_decay 1e-6 \
     --batch_size 128 \
     --num_workers 8 \
