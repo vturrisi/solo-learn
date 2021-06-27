045066e8fb3a108a4e92b237fe739192928a36e2
Victor Turrisi <vt.turrisi@gmail.com>
2021-06-27 15:05:04 +0000

    nnclr and byol

---
+++ bash_files/pretrain/cifar-100/byol.sh
@@ -0,0 +1,32 @@
+python3 ../../../main_contrastive.py \
+    --dataset cifar100 \
+    --encoder resnet18 \
+    --data_folder ./datasets \
+    --max_epochs 1000 \
+    --gpus 0 \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 1.0 \
+    --classifier_lr 0.1 \
+    --weight_decay 1e-5 \
+    --batch_size 256 \
+    --num_workers 5 \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.2 \
+    --hue 0.1 \
+    --asymmetric_augmentations \
+    --name byol-cifar100 \
+    --project solo-learn \
+    --entity unitn-mhug \
+    --wandb \
+    --method byol \
+    --output_dim 256 \
+    --proj_hidden_dim 4096 \
+    --pred_hidden_dim 4096 \
+    --base_tau_momentum 0.99 \
+    --final_tau_momentum 1.0 \
+    --momentum_classifier
