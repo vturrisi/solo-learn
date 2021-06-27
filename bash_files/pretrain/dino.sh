72f1b0692d6a64d6d9e712c03b21beb0699863eb
Enrico Fini <enrico.fini@gmail.com>
2021-06-27 15:05:35 +0200

    re-added dino

---
+++ bash_files/pretrain/dino.sh
@@ -0,0 +1,35 @@
+python3 ../../main_contrastive.py \
+    --dataset imagenet100 \
+    --encoder resnet18 \
+    --data_folder /data/datasets \
+    --train_dir imagenet-100/train \
+    --val_dir imagenet-100/val \
+    --max_epochs 500 \
+    --gpus 0,1 \
+    --distributed_backend ddp \
+    --sync_batchnorm \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.3 \
+    --classifier_lr 0.1 \
+    --weight_decay 1e-6 \
+    --batch_size 128 \
+    --num_workers 8 \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.2 \
+    --hue 0.1 \
+    --asymmetric_augmentations \
+    --name dino \
+    --entity unitn-mhug \
+    --project contrastive_learning \
+    --wandb \
+    --method dino \
+    --output_dim 256 \
+    --proj_hidden_dim 2048 \
+    --num_prototypes 4096 \
+    --base_tau_momentum 0.9995 \
+    --final_tau_momentum 1.0
