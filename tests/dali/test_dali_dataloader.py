c7b1171ba9dd627aa64a89c3b8d8a7a89056b9c0
Victor Turrisi <vt.turrisi@gmail.com>
2021-07-07 17:41:55 +0000

    added tests for dali

--- tests/dali/test_dali_dataloader.py
+++ tests/dali/test_dali_dataloader.py
@@ -5,7 +5,12 @@ import shutil
 
 import numpy as np
 from PIL import Image
-from solo.utils.dali_dataloader import ImagenetTransform
+from solo.utils.dali_dataloader import (
+    ContrastivePipeline,
+    ImagenetTransform,
+    MulticropContrastivePipeline,
+    NormalPipeline,
+)
 
 
 class DummyDataset:
@@ -39,15 +44,108 @@ class DummyDataset:
 def test_dali_dataloader():
     # creates a dummy dataset that autodeletes after usage
     with DummyDataset("dummy_train", "dummy_val", 10, 4):
-        transform = ImagenetTransform(
-            device="gpu",
-            brightness=0.4,
-            contrast=0.4,
-            saturation=0.2,
-            hue=0.1,
-            gaussian_prob=0.5,
-            solarization_prob=0.1,
-            size=224,
-            min_scale=0.08,
-            max_scale=0.08,
+        n_crops = [2, 4]
+        size_crops = [224, 96]
+        min_scale_crops = [0.14, 0.05]
+        max_scale_crops = [1.0, 0.14]
+
+        transforms = []
+        for size, min_scale, max_scale in zip(size_crops, min_scale_crops, max_scale_crops):
+            transform = ImagenetTransform(
+                device="cpu",
+                brightness=0.4,
+                contrast=0.4,
+                saturation=0.2,
+                hue=0.1,
+                gaussian_prob=0.5,
+                solarization_prob=0.1,
+                size=size,
+                min_scale=min_scale,
+                max_scale=max_scale,
+            )
+            transforms.append(transform)
+
+        # multicrop pipeline
+        train_pipeline = MulticropContrastivePipeline(
+            "dummy_train",
+            batch_size=4,
+            transforms=transforms,
+            n_crops=n_crops,
+            size_crops=size_crops,
+            min_scale_crops=min_scale_crops,
+            max_scale_crops=max_scale_crops,
+            device="cpu",
+            device_id=0,
+            shard_id=0,
+            num_shards=1,
+            num_threads=1,
         )
+        train_pipeline.build()
+
+        # output_map = [
+        #     *[f"large{i}" for i in range(n_crops[0])],
+        #     *[f"small{i}" for i in range(n_crops[1])],
+        #     "label",
+        # ]
+        # policy = LastBatchPolicy.DROP
+        # ContrastiveWrapper(
+        #     train_pipeline,
+        #     output_map=output_map,
+        #     reader_name="Reader",
+        #     last_batch_policy=policy,
+        #     auto_reset=True,
+        #     model_batch_size=4,
+        #     model_rank=0,
+        #     model_device="cpu",
+        # )
+
+        # simple contrastive pipeline
+        train_pipeline = ContrastivePipeline(
+            "dummy_train",
+            batch_size=4,
+            transform=transforms[0],
+            device="cpu",
+            device_id=0,
+            shard_id=0,
+            num_shards=1,
+            num_threads=1,
+        )
+        train_pipeline.build()
+
+        # output_map = ["large1", "large2", "label"]
+        # policy = LastBatchPolicy.DROP
+        # train_loader = ContrastiveWrapper(
+        #     train_pipeline,
+        #     output_map=output_map,
+        #     reader_name="Reader",
+        #     last_batch_policy=policy,
+        #     auto_reset=True,
+        #     model_batch_size=4,
+        #     model_rank=0,
+        #     model_device="cpu",
+        # )
+
+        # normal pipeline
+        train_pipeline = NormalPipeline(
+            "dummy_train",
+            validation=False,
+            batch_size=4,
+            device="cpu",
+            device_id=0,
+            shard_id=0,
+            num_shards=1,
+            num_threads=1,
+        )
+        train_pipeline.build()
+
+        val_pipeline = NormalPipeline(
+            "dummy_val",
+            validation=True,
+            batch_size=4,
+            device="cpu",
+            device_id=0,
+            shard_id=0,
+            num_shards=1,
+            num_threads=1,
+        )
+        val_pipeline.build()
