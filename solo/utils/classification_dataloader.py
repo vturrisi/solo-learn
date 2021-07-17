a44be2a4c9d05e210cdc83f125d44a3c487e2c87
Victor Turrisi <vt.turrisi@gmail.com>
2021-07-16 22:36:01 -0300

    black

--- solo/utils/classification_dataloader.py
+++ solo/utils/classification_dataloader.py
@@ -123,31 +123,19 @@ def prepare_datasets(
     if dataset in ["cifar10", "cifar100"]:
         DatasetClass = vars(torchvision.datasets)[dataset.upper()]
         train_dataset = DatasetClass(
-            os.path.join(data_dir, train_dir),
-            train=True,
-            download=True,
-            transform=T_train,
+            os.path.join(data_dir, train_dir), train=True, download=True, transform=T_train,
         )
 
         val_dataset = DatasetClass(
-            os.path.join(data_dir, val_dir),
-            train=False,
-            download=True,
-            transform=T_val,
+            os.path.join(data_dir, val_dir), train=False, download=True, transform=T_val,
         )
 
     elif dataset == "stl10":
         train_dataset = STL10(
-            os.path.join(data_dir, train_dir),
-            split="train",
-            download=True,
-            transform=T_train,
+            os.path.join(data_dir, train_dir), split="train", download=True, transform=T_train,
         )
         val_dataset = STL10(
-            os.path.join(data_dir, val_dir),
-            split="test",
-            download=True,
-            transform=T_val,
+            os.path.join(data_dir, val_dir), split="test", download=True, transform=T_val,
         )
 
     elif dataset in ["imagenet", "imagenet100"]:
@@ -219,17 +207,9 @@ def prepare_data(
 
     T_train, T_val = prepare_transforms(dataset)
     train_dataset, val_dataset = prepare_datasets(
-        dataset,
-        T_train,
-        T_val,
-        data_dir=data_dir,
-        train_dir=train_dir,
-        val_dir=val_dir,
+        dataset, T_train, T_val, data_dir=data_dir, train_dir=train_dir, val_dir=val_dir,
     )
     train_loader, val_loader = prepare_dataloaders(
-        train_dataset,
-        val_dataset,
-        batch_size=batch_size,
-        num_workers=num_workers,
+        train_dataset, val_dataset, batch_size=batch_size, num_workers=num_workers,
     )
     return train_loader, val_loader
