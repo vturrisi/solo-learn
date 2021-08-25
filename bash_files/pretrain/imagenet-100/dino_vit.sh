1ff0c3733cf6e76ffdd0125fa35b801df6d46cff
Victor Turrisi <vt.turrisi@gmail.com>
2021-08-24 23:02:46 -0300

    added dino_vit script

---
+++ bash_files/pretrain/imagenet-100/dino_vit.sh
@@ -0,0 +1,38 @@
+python3 ../../../main_pretrain.py \
+    --dataset imagenet100 \
+    --encoder vit_tiny \
+    --data_dir /data/datasets \
+    --train_dir imagenet-100/train \
+    --val_dir imagenet-100/test \
+    --max_epochs 400 \
+    --gpus 0,1 \
+    --distributed_backend ddp \
+    --sync_batchnorm \
+    --precision 16 \
+    --optimizer sgd \
+    --lars \
+    --grad_clip_lars \
+    --eta_lars 0.02 \
+    --exclude_bias_n_norm \
+    --scheduler warmup_cosine \
+    --lr 0.003 \
+    --classifier_lr 0.1 \
+    --weight_decay 1e-4 \
+    --batch_size 128 \
+    --num_workers 8 \
+    --dali \
+    --brightness 0.4 \
+    --contrast 0.4 \
+    --saturation 0.2 \
+    --hue 0.1 \
+    --gaussian_prob 1.0 0.1 \
+    --solarization_prob 0.0 0.2 \
+    --name vit_tiny-dino-400ep-imagenet100 \
+    --project solo-learn \
+    --wandb \
+    --method dino \
+    --output_dim 65536 \
+    --proj_hidden_dim 2048 \
+    --num_prototypes 4096 \
+    --base_tau_momentum 0.9995 \
+    --final_tau_momentum 1.0
