9f1b1315207a2cea66167358565254e60a03b0b6
Enrico Fini <enrico.fini@gmail.com>
2021-09-06 13:46:37 +0200

    added bash file for byol on full imagenet (#132)

--- bash_files/linear/imagenet-100/nnclr_linear.sh
+++ bash_files/linear/imagenet-100/nnclr_linear.sh
@@ -1,7 +1,7 @@
-python3 ../../main_linear.py \
+python3 ../../../main_linear.py \
     --dataset imagenet100 \
     --encoder resnet18 \
-    --data_dir /data/datasets \
+    --data_dir /data0/SHARED/datasets \
     --train_dir imagenet-100/train \
     --val_dir imagenet-100/val \
     --max_epochs 100 \
@@ -9,13 +9,13 @@ python3 ../../main_linear.py \
     --precision 16 \
     --optimizer sgd \
     --scheduler step \
-    --lr 0.15 \
+    --lr 0.3 \
     --lr_decay_steps 60 80 \
     --weight_decay 0 \
     --batch_size 256 \
-    --num_workers 5 \
+    --num_workers 10 \
     --dali \
-    --name swav-imagenet100-linear-eval \
+    --name nnclr-imagenet100-linear-eval \
     --pretrained_feature_extractor PATH \
     --project solo-learn \
     --entity unitn-mhug \
