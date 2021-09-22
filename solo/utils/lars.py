5cee4a097eae02c9b6ff72c702e6f806ccbe9c51
Victor Turrisi <vt.turrisi@gmail.com>
2021-09-22 14:51:38 -0300

    added license to all files (#143)

--- solo/utils/lars.py
+++ solo/utils/lars.py
@@ -1,9 +1,26 @@
-"""
-References:
-    - https://github.com/NVIDIA/apex/blob/master/apex/parallel/LARC.py
-    - https://arxiv.org/pdf/1708.03888.pdf
-    - https://github.com/noahgolmant/pytorch-lars/blob/master/lars.py
-"""
+# Copyright 2021 solo-learn development team.
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies
+# or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+# Copied from Pytorch Lightning (https://github.com/PyTorchLightning/pytorch-lightning/)
+# with extra documentations.
+
+
 import torch
 from torch.optim import Optimizer
 
--- tests/dali/test_dali_model.py
+++ tests/dali/test_dali_model.py
@@ -1,9 +1,28 @@
-import argparse
-from nvidia.dali.plugin.base_iterator import LastBatchPolicy
+# Copyright 2021 solo-learn development team.
 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+# Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies
+# or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+# FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import argparse
+
+from nvidia.dali.plugin.base_iterator import LastBatchPolicy
 from pytorch_lightning import Trainer
 from solo.methods import BarlowTwins
-from solo.methods.dali import ClassificationABC, PretrainABC, NormalPipeline, Wrapper
+from solo.methods.dali import ClassificationABC, NormalPipeline, PretrainABC, Wrapper
 from solo.methods.linear import LinearModel
 from torch import nn
 from torchvision.models.resnet import resnet18
