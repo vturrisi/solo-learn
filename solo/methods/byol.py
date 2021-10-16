fixed std calculation in byol

solo/methods/byol.py
@@ -145,7 +145,7 @@ class BYOL(BaseMomentumMethod):
 
         # calculate std of features
         with torch.no_grad():
-            z_std = F.normalize(torch.stack(Z[: self.num_large_crops]), dim=-1).std(dim=0).mean()
+            z_std = F.normalize(torch.stack(Z[: self.num_large_crops]), dim=-1).std(dim=1).mean()
 
         return neg_cos_sim, z_std
 