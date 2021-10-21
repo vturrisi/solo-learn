3e1e5ac545279fa9d279cdc0752abf438380cfa5
Victor Turrisi <vt.turrisi@gmail.com>
2021-10-21 17:00:59 +0000

    batch size

--- other_tasks/object_detection/run.sh
+++
@@ -1,4 +0,0 @@
-export DETECTRON2_DATASETS=/data/datasets
-
-python3 train_object_detection.py --config-file configs/pascal_voc_R_50_C4_24k_moco.yaml \
-    --num-gpus 2 MODEL.WEIGHTS ./detectron_model.pkl
