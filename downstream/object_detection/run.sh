b441e7088865d6d1ef0b63986091850fa43457cb
Victor Turrisi <vt.turrisi@gmail.com>
2021-10-21 20:59:08 -0300

    Added support for Detectron2 (object recognition) (#170)

---
+++ downstream/object_detection/run.sh
@@ -0,0 +1,4 @@
+export DETECTRON2_DATASETS=/data/datasets
+
+python3 train_object_detection.py --config-file configs/pascal_voc_R_50_C4_24k_moco.yaml \
+    --num-gpus 2 MODEL.WEIGHTS ./detectron_model.pkl SOLVER.IMS_PER_BATCH 2
