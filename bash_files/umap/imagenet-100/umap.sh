95886b681f2f29adc4ebd418f0a5ba2957bfbada
Victor Turrisi <vt.turrisi@gmail.com>
2021-11-29 18:42:22 +0100

    Improvement/umap (#199)

---
+++ bash_files/umap/imagenet-100/umap.sh
@@ -0,0 +1,8 @@
+python3 ../../../main_umap.py \
+    --dataset imagenet100 \
+    --data_dir /datasets \
+    --train_dir imagenet-100/train \
+    --val_dir imagenet-100/val \
+    --batch_size 16 \
+    --num_workers 10 \
+    --pretrained_checkpoint_dir PATH
--- solo/args/setup.py
+++ solo/args/setup.py
@@ -136,6 +136,11 @@ def parse_args_linear() -> argparse.Namespace:
 
 
 def parse_args_knn() -> argparse.Namespace:
+    """Parses arguments for offline K-NN.
+
+    Returns:
+        argparse.Namespace: a namespace containing all args needed for pretraining.
+    """
 
     parser = argparse.ArgumentParser()
 
@@ -147,6 +152,30 @@ def parse_args_knn() -> argparse.Namespace:
     parser.add_argument("--temperature", type=float, nargs="+")
     parser.add_argument("--distance_function", type=str, nargs="+")
     parser.add_argument("--feature_type", type=str, nargs="+")
+
+    # add shared arguments
+    dataset_args(parser)
+    custom_dataset_args(parser)
+
+    # parse args
+    args = parser.parse_args()
+
+    return args
+
+
+def parse_args_umap() -> argparse.Namespace:
+    """Parses arguments for offline UMAP.
+
+    Returns:
+        argparse.Namespace: a namespace containing all args needed for pretraining.
+    """
+
+    parser = argparse.ArgumentParser()
+
+    # add knn args
+    parser.add_argument("--pretrained_checkpoint_dir", type=str)
+    parser.add_argument("--batch_size", type=int, default=16)
+    parser.add_argument("--num_workers", type=int, default=10)
 
     # add shared arguments
     dataset_args(parser)
