51b100210f0aaf72b8548783594e03895fa3cd1a
Enrico Fini <enrico.fini@gmail.com>
2021-12-20 10:02:58 +0100

    added mocov2plus to zoo

--- zoo/imagenet.sh
+++ zoo/imagenet.sh
@@ -9,3 +9,10 @@ cd byol
 gdown https://drive.google.com/uc?id=1TheL_4tmDWByCxg8XHke5VEz_lcYHH64
 gdown https://drive.google.com/uc?id=18gG0Jo59cFVX4qNUO119jIhzHcJkAmGz
 cd ..
+
+# MoCo V2+
+mkdir mocov2plus
+cd mocov2plus
+gdown https://drive.google.com/uc?id=1BBauwWTJV38BCf56KtOK9TJWLyjNH-mP
+gdown https://drive.google.com/uc?id=1JMpGSYjefFzxT5GTbEc_2d4THxOxC3Ca
+cd ..
