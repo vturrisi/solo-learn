445b50950b5d82a652b38e68600b343a49a609cd
Enrico Fini <enrico.fini@gmail.com>
2021-12-20 09:28:38 +0100

    added linear eval script for mocov2plus

---
+++ bash_files/linear/imagenet/mocov2plus.sh
@@ -0,0 +1,23 @@
+python3 main_linear.py \
+    --dataset imagenet \
+    --backbone resnet50 \
+    --data_dir /data/datasets \
+    --train_dir imagenet/train \
+    --val_dir imagenet/val \
+    --max_epochs 100 \
+    --gpus 0 \
+    --precision 16 \
+    --optimizer sgd \
+    --scheduler step \
+    --lr 3.0 \
+    --lr_decay_steps 60 80 \
+    --weight_decay 0 \
+    --batch_size 256 \
+    --num_workers 5 \
+    --dali \
+    --name mocov2plus-imagenet-linear-eval \
+    --pretrained_feature_extractor PATH \
+    --project solo-learn \
+    --entity unitn-mhug \
+    --wandb \
+    --save_checkpoint
