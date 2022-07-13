75a34b03b98070b31f8600d7974b96e2062562f9
Victor Turrisi <vt.turrisi@gmail.com>
2022-07-13 22:01:07 +0000

    fix knn and umap scripts

--- scripts/knn/imagenet-100/knn.sh
+++ scripts/knn/imagenet-100/knn.sh
@@ -1,8 +1,7 @@
 python3 main_knn.py \
     --dataset imagenet100 \
-    --data_dir /datasets \
-    --train_dir imagenet-100/train \
-    --val_dir imagenet-100/val \
+    --train_data_path /datasets/imagenet-100/train \
+    --val_data_path /datasets/imagenet-100/val \
     --batch_size 16 \
     --num_workers 10 \
     --pretrained_checkpoint_dir PATH \
