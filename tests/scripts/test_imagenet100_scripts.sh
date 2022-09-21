3a64e5587bac0441c8f86f8b2a1f54773440c30a
Victor Turrisi <vt.turrisi@gmail.com>
2022-09-21 15:12:29 +0100

    fix cifar scripts

--- tests/scripts/test_imagenet100_scripts.sh
+++ tests/scripts/test_imagenet100_scripts.sh
@@ -4,22 +4,22 @@ FORMAT=$3
 METHODS=("barlow" "byol" "dino" "dino_vit" "mae" "mocov2plus" "mocov3" "nnclr" "ressl" "simclr" "simsiam" "supcon" "swav" "vibcreg" "vicreg")
 
 for method in ${METHODS[@]}; do
-echo Running $method
-python3 main_pretrain.py \
-    --config-path scripts/pretrain/imagenet-100/ \
-    --config-name $method \
-    max_epochs=2 \
-    devices=[0] \
-    optimizer.batch_size=32 \
-    data.train_path=$TRAIN_PATH \
-    data.val_path=$VAL_PATH \
-    data.format=$FORMAT \
-    ++auto_resume.enabled=False \
-    ++wandb.enabled=False \
-    ++limit_train_batches=30 \
-    ++limit_val_batches=30 \
-    ++method_kwargs.warmup_teacher_temperature_epochs=0
-echo ---------------------------------
+    echo Running $method
+    python3 main_pretrain.py \
+        --config-path scripts/pretrain/imagenet-100/ \
+        --config-name $method \
+        max_epochs=2 \
+        devices=[0] \
+        optimizer.batch_size=32 \
+        data.train_path=$TRAIN_PATH \
+        data.val_path=$VAL_PATH \
+        data.format=$FORMAT \
+        ++auto_resume.enabled=False \
+        ++wandb.enabled=False \
+        ++limit_train_batches=30 \
+        ++limit_val_batches=30 \
+        ++method_kwargs.warmup_teacher_temperature_epochs=0
+    echo ---------------------------------
 done
 
 # run wmse with larger batch
