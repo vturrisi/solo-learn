f86c9c6f732127ea94b23df9480b16766a38fae2
Jirka <jirka.borovec@seznam.cz>
2022-12-31 17:02:35 +0100

    unify requirements

--- setup.py
+++ setup.py
@@ -16,11 +16,16 @@
 # FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
+import os
 
+from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
 KW = ["artificial intelligence", "deep learning", "unsupervised learning", "contrastive learning"]
 
+REQUIREMENTS_FILE = os.path.join(os.path.dirname(__file__), "requirements.txt")
+with open(REQUIREMENTS_FILE) as fo:
+    REQUIREMENTS = list(parse_requirements(fo.readlines()))
 
 EXTRA_REQUIREMENTS = {
     "dali": ["nvidia-dali-cuda110"],
@@ -44,20 +49,7 @@ setup(
     author_email="vturrisi@gmail.com, enrico.fini@gmail.com",
     url="https://github.com/vturrisi/solo-learn",
     keywords=KW,
-    install_requires=[
-        "torch>=1.10.0",
-        "torchvision>=0.11.1",
-        "einops",
-        "pytorch-lightning>=1.7.0, <1.9.0",
-        "torchmetrics>=0.6.0, <0.12.0",
-        "lightning-bolts>=0.6.0",
-        "tqdm",
-        "wandb",
-        "scipy",
-        "timm",
-        "scikit-learn",
-        "hydra-core",
-    ],
+    install_requires=REQUIREMENTS,
     extras_require=EXTRA_REQUIREMENTS,
     dependency_links=["https://developer.download.nvidia.com/compute/redist"],
     classifiers=[
