08e10899330f9e125b65a06159fc1ecde3d67853
Santiago Castro <sacastro@umich.edu>
2023-05-25 14:28:57 -0400

    Add explicit support for Python 3.10 and 3.11

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
